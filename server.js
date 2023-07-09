b0c9705a01f252b0a44cfe7d7edc2873d11be2a0
LukeWellsey <46170241+LukeWellsey@users.noreply.github.com>
2023-07-08 18:01:17 -0600

    Update server.js
    do the thing

--- server.js
+++ server.js
@@ -13,4 +13,9 @@ app.get('/luke', (req,res) => {res.send("Hello Luke")});
 //I could just keep going
 app.get('/luke/bacon', (req,res) => {res.send("you found the bacon place")});
 
+//made these changes in github, now I'll pull them into my local git repo
+app.get('/sean', (req,res) => {res.send("Hello Sean")}); 
+
+app.get('/bobbert', (req,res) => {res.send("Bob is short for Bobbert")}); 
+
 app.listen(port, ()=>console.log("Listening"));
